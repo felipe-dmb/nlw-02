830cc7fff667ddaeb5f4b3583b10aa97a5b12e50
Felipe Barbosa <fekipedmb@gmail.com>
2020-08-06 01:50:08 -0300

    TeacherFom page strutured and styled. Three new components: Select, Input, Textarea.

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,13 +1,93 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas."/>
+      <PageHeader 
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+          <Textarea name="bio" label="Biografia" />
+
+        </fieldset>
+        <fieldset>
+          <legend>Sobre a Aula</legend>
+
+          <Select 
+            name="subject" 
+            label="Matéria"
+            options={[
+              {value: 'Artes', label:'Artes'},
+              {value: 'Ciência', label:'Ciência'},
+              {value: 'Biologia', label:'Biologia'},
+              {value: 'Matemática', label:'Matemática'},
+              {value: 'Português', label:'Português'},
+              {value: 'História', label:'História'},
+              {value: 'Geografia', label:'Geografia'}
+            ]}
+          />
+          <Input name="cost" label="Custo da sua Hora por aula" />
+
+        </fieldset>
+        <fieldset>
+          <legend>
+            Horários Disponíveis
+            <button type="button">
+              + Novo Horário
+            </button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select 
+              name="week_day" 
+              label="Dia da Semana"
+              options={[
+                {value: '0', label:'Domingo'},
+                {value: '1', label:'Segunda-feira'},
+                {value: '2', label:'Terça-feira'},
+                {value: '3', label:'Quarta-feira'},
+                {value: '4', label:'Quinta-feira'},
+                {value: '5', label:'Sexta-feira'},
+                {value: '6', label:'Sábado'}
+              ]}
+            />
+            <Input name="from" type="time" label="Das"/>
+            <Input name="to" type="time" label="Até"/>
+          </div>
+        </fieldset>
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso"/>
+            Importante <br/>
+            Preencha todos os Dados
+          </p>
+          <button type="button">
+            Salvar Cadastro
+          </button>
+
+          
+
+          
+
+        </footer>
+      </main>
+
     </div>
   )
 }
